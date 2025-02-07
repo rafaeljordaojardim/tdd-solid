5aea6358a4bee5345053a7aa6948ea469035a201
Rafael Jardim <jordao.jardim@hotmail.com.br>
2025-02-07 06:51:15 -0300

    feat: ensure SignupController returns 400 is no name is provided

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import love from 'eslint-config-love';
 export default [
   {
     ...love,
-    files: ['src/*.js', 'src/*.ts'],
+    files: ['src/**'],
     rules: {
       "@typescript-eslint/strict-boolean-expressions": "off",
 
