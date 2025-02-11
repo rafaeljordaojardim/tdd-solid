c7c77f7b3283e792f659fa11d87c7f453f941efe
Rafael Jardim <jordao.jardim@hotmail.com.br>
2025-02-10 21:31:08 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { MissingParamError } from "../erros/missing-param-error";
 import { SignUpController } from "./signup";
 
+const makeSut = () => {
+  return new SignUpController()
+}
+
 describe('Signup controller', () => {
   it('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -17,7 +21,7 @@ describe('Signup controller', () => {
   });
 
   it('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -31,7 +35,7 @@ describe('Signup controller', () => {
   });
 
   it('Should return 400 if no password is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -45,7 +49,21 @@ describe('Signup controller', () => {
   });
 
   it('Should return 400 if no password confirmation is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_pasword'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  });
+
+  it('should return 400 if an email is not passed correctly', () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
