4581b3d61b8b62b21e3b9d35e918099887a303d4
Rafael Jardim <jordao.jardim@hotmail.com.br>
2025-02-15 21:56:19 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account";
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async()=>{
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  
+  afterAll(async()=>{
+    await MongoHelper.disconnect()
+  })
+
+  it('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'test',
+      email: 'test@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('test')
+    expect(account.email).toBe('test@mail.com')
+    expect(account.password).toBe('any_password')
+  });
+});
