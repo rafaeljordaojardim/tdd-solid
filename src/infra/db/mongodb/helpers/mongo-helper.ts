4fbb431f41ec249553473b5f95e90e21d49425bc
Rafael Jardim <jordao.jardim@hotmail.com.br>
2025-02-17 06:42:48 -0300

    refactor: connect to database before app listen

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,14 +3,14 @@ import { Collection, MongoClient } from "mongodb";
 export const MongoHelper = {
   client: null as MongoClient,
   async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL)
+    this.client = await MongoClient.connect(uri)
 
   },
   async disconnect() {
     this.client.close()
 
   },
-
+  
   getCollection(name: string): Collection {
     return this.client.db().collection(name)
   }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,10 @@
-import app from "./config/app"
+import { MongoHelper } from "../infra/db/mongodb/helpers/mongo-helper"
+import env from './config/env'
 
-app.listen(5050, () => console.log("server Running"))
+MongoHelper.connect(env.mongoUrl)
+.then(async () => {
+    console.log(`Connected to database`);
+    const app = (await import("./config/app")).default
+    app.listen(env.port, () => console.log("server Running"))
+  })
+  .catch((err) => console.log(err))
