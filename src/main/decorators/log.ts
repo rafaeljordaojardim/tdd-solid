433d08eff3980a8ea831ab826ab0025b91ec91e6
Rafael Jardim <jordao.jardim@hotmail.com.br>
2025-02-18 21:49:24 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository when returns 500

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,13 +1,14 @@
+import { LogErrorRepository } from "../../data/protocols/log-error-repository";
 import { Controller, httpRequest, httpResponse } from "../../presentation/protocols";
 
 export class LogControllerDecorator implements Controller {
-  constructor(private readonly contoller: Controller) {
+  constructor(private readonly contoller: Controller, private readonly logErrorRepository: LogErrorRepository) {
 
   }
   async handle(httpRequest: httpRequest): Promise<httpResponse> {
     const httpResponse = await this.contoller.handle(httpRequest)
     if (httpResponse.statusCode == 500) {
-      console.log(`Some logger`);
+      await this.logErrorRepository.log(httpRequest.body.stack)
     }
     return httpResponse
 
