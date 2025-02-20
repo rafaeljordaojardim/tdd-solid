9bd9e2a23c1bb6c0124a3b89532e7f011a70f611
Rafael Jardim <jardemr@vmware.com>
2025-02-20 06:36:53 -0300

    refactor: change field validation

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -4,12 +4,11 @@ import { Controller, httpRequest, httpResponse } from "../../protocols";
 
 export class LoginController implements Controller{
   async handle(httpRequest: httpRequest): Promise<httpResponse> {
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
-    if (!httpRequest.body.password) {
-      return badRequest(new MissingParamError('password'))
+    const fields = ['email', 'password']
+    for (const field of fields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 
