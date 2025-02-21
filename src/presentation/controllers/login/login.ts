ab3a88ed3ecde925d1f57b5817e8c2e9f40aaf60
Rafael Jardim <jordao.jardim@hotmail.com.br>
2025-02-21 05:12:23 -0300

    feat: ensure emailValidator is called with correct param

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,12 @@
 import { MissingParamError } from "../../erros";
 import { badRequest } from "../../helpers/http-helper";
 import { Controller, httpRequest, httpResponse } from "../../protocols";
+import { EmailValidator } from "../signup/signup-protocols";
 
 export class LoginController implements Controller{
+  constructor(
+    private readonly emailValidator: EmailValidator
+  ) {}
   async handle(httpRequest: httpRequest): Promise<httpResponse> {
     const fields = ['email', 'password']
     for (const field of fields) {
@@ -10,6 +14,7 @@ export class LoginController implements Controller{
         return badRequest(new MissingParamError(field))
       }
     }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
   }
 
 }
