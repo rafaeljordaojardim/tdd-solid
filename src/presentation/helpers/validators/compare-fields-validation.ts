65353ebe2c1be5e0092cf9ed61dc04f039670d0f
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2025-02-25 06:14:08 -0300

    refactor: move validator to protocols folder

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../erros";
-import { Validation } from "./validation";
+import { Validation } from "../../protocols/validation";
 
 export class CompareFieldsValidation implements Validation {
   constructor(
--- src/presentation/helpers/validators/email-validation.ts
+++ src/presentation/helpers/validators/email-validation.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError, MissingParamError } from "../../erros";
 import { EmailValidator } from "../../protocols/email-validator";
-import { Validation } from "./validation";
+import { Validation } from "../../protocols/validation";
 
 export class EmailValidation implements Validation {
   constructor(
