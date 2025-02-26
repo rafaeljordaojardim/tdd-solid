44c5e84dcba159ed2864b4bb1ffedad5c8b28074
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2025-02-26 07:09:12 -0300

    feat: ensure DBAuthentication returns null if password is not valid

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,12 +1,23 @@
 import { Authentication } from "../../../domain/usecases/authentication";
+import { HashComparer } from "../../protocols/criptography/hash-comparer";
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 
 export class DbAuthentication implements Authentication {
 
-  constructor(private loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor(
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
+  ) {}
  async auth(email: string, password: string): Promise<string> {
-    await this.loadAccountByEmailRepository.load(email)
-    return null
+    const user = await this.loadAccountByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
+    const isValid = await this.hashComparer.compare(password, user.password)
+
+    if (!isValid) {
+      return null
+    }
   }
 
 }
