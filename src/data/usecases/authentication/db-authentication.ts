e6b8fe7977af54c4361659d8f4abbcc06150269b
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2025-02-26 06:44:10 -0300

    feat: ensure DBAuthentication calls load acccount by email repository

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,12 @@
+import { Authentication } from "../../../domain/usecases/authentication";
+import { LoadAccountByEmailRepository } from "../../protocols/load-account-by-email-repository";
+
+export class DbAuthentication implements Authentication {
+
+  constructor(private loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+ async auth(email: string, password: string): Promise<string> {
+    await this.loadAccountByEmailRepository.load(email)
+    return null
+  }
+
+}
